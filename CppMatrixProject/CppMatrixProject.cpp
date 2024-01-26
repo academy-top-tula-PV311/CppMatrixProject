97baba76286df1cdb21b3949af5e67d0806ffa92
max <efimov.top.academy@gmail.com>
2024-01-26 21:17:22 +0300

    Добавьте файлы проекта.

---
+++ CppMatrixProject/CppMatrixProject.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    const int rows{ 4 };
+    const int cols{ 6 };
+
+    int matrix[rows][cols];
+
+    int count{ 0 };
+
+    for (int i{}; i < rows; i++)
+    {
+        count += 10;
+        for (int j{}; j < cols; j++)
+            matrix[i][j] = ++count;
+            //matrix[i][j] = (i + 1) * 10 + (j + 1);
+        count -= cols;
+    }
+        
+
+    for (int i{}; i < rows; i++)
+    {
+        for (int j{}; j < cols; j++)
+            std::cout << std::setw(4) << matrix[i][j]; // << "\t";
+        std::cout << "\n";
+    }
+        
+    std::cout << "\n";
+
+    /*bool f{ true };
+    std::cout << std::boolalpha << f << std::endl;*/
+
+}
